aa306bc33d18d07a5fc7e4b6e352547b95ecef40
Rahul Kawari <kawarirahul@gmail.com>
2022-03-02 16:46:18 +0545

    changed code to make more it cleaner

--- server/modules/Insertion/routesInsertion.js
+++ server/modules/Insertion/routesInsertion.js
@@ -1,13 +1,15 @@
-const { insertionService } = require(".");
+const { getTable } = require("./serviceInsertion");
 
-module.exports = (app, routes) => {
-  app.get('/show', (req, res) => {
+module.exports = (app, conn) => {
+  
+  app.get('/show', async (req, res) => {
 
     try{
-      const details = insertionService.getTable();
+      const details = await getTable(conn);
       res.json(details);
     }
     catch(error){
+      console.log(error)
         res.json({status:'error', message: error.message});
     }
 
--- server/modules/Insertion/serviceInsertion.js
+++ server/modules/Insertion/serviceInsertion.js
@@ -1,6 +1,9 @@
-const insertionDal = require('./dalInsertion');
-const serviceInsertion = {
-  getTable: () => {
-    insertionDal.getTable();
-  }
+'use strict';
+const { DALObj } = require('./dalInsertion');
+
+let getTable =async (conn) => {
+    let obj = await DALObj.getTable(conn)    
+    return obj.result;
 }
+
+module.exports = { getTable }
